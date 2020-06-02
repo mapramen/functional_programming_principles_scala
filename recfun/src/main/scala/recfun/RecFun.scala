3893fa5e632ae42e555f99c62f73b7751cb60137
Manvendra Kumar <manvendra.kumar7@gmail.com>
2020-06-03 04:16:08 +0530

    Week 1 - Recursion: Completed!

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -14,15 +14,38 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 
+    if(r < 0) 0
+    else if(r < c) 0
+    else if(c == 0) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def charToInt(c: Char): Int = c match {
+      case '(' => 1
+      case ')' => -1
+      case _ => 0
+    }
+
+    def checkValidParenthesisHelper(acc: Int, s: List[Char]): Boolean = {
+      if(acc < 0) false
+      else if(s == Nil) acc == 0
+      else checkValidParenthesisHelper(acc + charToInt(s.head), s.tail)
+    }
+
+    checkValidParenthesisHelper(0, chars);
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money < 0) 0
+    else if(money == 0) 1
+    else if(coins == Nil) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
 }
